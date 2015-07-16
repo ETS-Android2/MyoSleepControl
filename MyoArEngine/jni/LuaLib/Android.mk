f6ae04fb43db9cbaa7ab5cc27d865beeda9f9c15
Bernd Herzog <bernd.herzog@nachregenkommtsonne.de>
2015-07-16 23:21:27 +0200

    restructuring

--- MyoArEngine/jni/LuaLib/Android.mk
+++ MyoArEngine/jni/LuaLib/Android.mk
@@ -1,16 +1,29 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
+#include $(CLEAR_VARS)
+#
+#LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua
+#LOCAL_C_INCLUDES += ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.8/include
+#
+#LOCAL_MODULE := MyoArEngine
+#
+#LOCAL_SRC_FILES := MyoArEngine.cpp # UIElement.cpp Scripting.cpp UIButton.cpp UIImage.cpp UILabel.cpp UITextField.cpp
+#
+#LOCAL_STATIC_LIBRARIES := liblua
+#LOCAL_LDLIBS := -lGLESv2
+#LOCAL_CPPFLAGS := -std=c++11
+#
+#include $(BUILD_SHARED_LIBRARY)
 
+
+
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua
-LOCAL_C_INCLUDES += ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.8/include
+LOCAL_CFLAGS += -std=c99
 
-LOCAL_MODULE := MyoArEngine
+LOCAL_MODULE    := AirGap
+LOCAL_SRC_FILES := AirGapMain.cpp
 
-LOCAL_SRC_FILES := MyoArEngine.cpp # UIElement.cpp Scripting.cpp UIButton.cpp UIImage.cpp UILabel.cpp UITextField.cpp
-
-LOCAL_STATIC_LIBRARIES := liblua
-LOCAL_LDLIBS := -lGLESv2
-LOCAL_CPPFLAGS := -std=c++11
+LOCAL_EXPORT_CFLAGS := -DANDROID=1
 
 include $(BUILD_SHARED_LIBRARY)
